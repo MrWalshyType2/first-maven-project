f01fd5361fe63950ebc1d60c42a77e76429a38a1
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-06-30 11:44:27 +0100

    added subtract method

--- src/main/java/com/qa/questions/Questions.java
+++ src/main/java/com/qa/questions/Questions.java
@@ -2,4 +2,5 @@ package com.qa.questions;
 
 public class Questions {
 	public int add(int num1, int num2) { return num1 + num2; }
+	public int subtract(int num1, int num2) { return num1 + num2; }
 }
